c9197e01f80672d4a1af6239f66f731581c67447
power <power@powerdeMacBook-Pro.local>
2023-04-02 23:33:25 +0800

    build: create vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ command }) => {
+  let base = ''
+  base = command === 'build' ? '/healtyH5' : './'
+  return{
+    base,
+    plugins: [vue()],
+    build: {
+      outDir: 'healtyH5'
+    }
+   
+  }
 })
