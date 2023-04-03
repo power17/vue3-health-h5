f4ff285e62f4b0d564b397bb541835ac7f48e81d
power <power@powerdeMacBook-Pro.local>
2023-04-04 01:38:49 +0800

    feat: wx bottom bar hide

--- src/main.js
+++ src/main.js
@@ -7,6 +7,17 @@ import './assets/animation.css'
 const app = createApp(App)
 app.mount('#app')
 // 隐藏微信底部
-document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-    WeixinJSBridge.call('hideToolbar');
-    });
+function onBridgeReady(){
+    WeixinJSBridge.call('hideOptionMenu');
+   }
+   
+   if (typeof WeixinJSBridge == "undefined"){
+       if( document.addEventListener ){
+           document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+       }else if (document.attachEvent){
+           document.attachEvent('WeixinJSBridgeReady', onBridgeReady); 
+           document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+       }
+   }else{
+       onBridgeReady();
+   }
